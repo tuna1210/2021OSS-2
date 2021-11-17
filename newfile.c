4a5e52a696f5ae91bb68453ea19f25420b488ec0
Dong-Two <94510215+Dong-Two@users.noreply.github.com>
2021-11-17 14:27:18 +0900

    Make Conflict function

--- newfile.c
+++ newfile.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+void conflict()
+{
+    printf("This func will cause conflict by ID2");
+}
+
 int main()
 {
     printf("This is test file\n");
-    printf("Make Conflict - ID1");
+    printf("Make Conflict - ID2");
 
     return 0;
 }
