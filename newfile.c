f1afe6b16ce56a73686b79d557ea323c36a9bac6
Dongwon Jeong <jlove981210@gmail.com>
2021-11-17 14:27:23 +0900

    Update newfile.c

--- newfile.c
+++ newfile.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+void conflict()
+{
+    printf("This func will cause conflict");
+}
+
 int main()
 {
     printf("This is test file\n");
-    printf("Make Conflict - ID1");
 
     return 0;
 }
